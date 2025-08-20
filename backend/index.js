8f977e506b8377f3657ebea08ff253f31beee66c
MoeedSarwar1 <moeedsarwar112@gmail.com>
2025-08-20 19:04:02 +0500

    Add mongoose connection and update dependencies in backend

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,23 @@
 // index.js
 const express = require("express");
+const mongoose = require("mongoose");
+
 const cors = require("cors");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT;
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error("MongoDB connection error:", err);
+  });
 
 app.use(cors()); // Enable CORS for all routes
 app.use(express.json()); // Enable parsing JSON request bodies
